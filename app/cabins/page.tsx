99b42bf851a00ffb5fa5d69e69788fd958e953be
Eros Campos <eroscamposg@gmail.com>
2026-06-25 19:41:49 -0500

    First fetch and logo

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,3 +1,18 @@
-export default function CabinPage() {
-    return <div>Cabing page</div>
+import { User } from '@/types/user';
+
+export default async function CabinPage() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/');
+  const users: User[] = await res.json();
+
+  return (
+    <div>
+      <h1>Cabins page</h1>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import Link from 'next/link';
-import Navigation from './components/Navigation';
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Navigation />
       <h1>The Wild Oasis. Welcome to paradise!</h1>
     </div>
   );
