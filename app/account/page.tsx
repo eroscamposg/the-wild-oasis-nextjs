45fe5ff4b837b6c79430bdd51ed1daefbfa6aaa6
Eros Campos <eroscamposg@gmail.com>
2026-06-29 13:19:57 -0500

    Metadata, favicons and tailwind globals

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Account',
+};
+
 export default function AccountPage() {
-    return <div>Account page</div>
+  return <div>Account page</div>;
 }
